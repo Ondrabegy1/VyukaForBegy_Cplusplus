c73d092a188e584d65866f2e27703e8df411e733
Ondrabegy1 <115577318+Ondrabegy1@users.noreply.github.com>
2023-10-16 10:37:33 +0200

    Add files via upload

---
+++ Stromeček/Stromeček/Stromeček.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void vytvoreniKmene(int vyska, int sirka, int vyska_stromu)
+{
+    for (int i = 0; i < vyska; i++)
+    {
+        for (int j = 0; j < vyska_stromu - sirka / 2; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < sirka; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void vytvoreniKoruny(int vyska)
+{
+    for (int i = 0; i < vyska; i++)
+    {
+        for (int j = 0; j < vyska - i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int k = 0; k < 2 * i + 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+
+
+int main()
+{
+    int vyska_stromu;
+    cout << "Zadejte vysku stromu: ";
+    cin >> vyska_stromu;
+
+    int sirka;
+    cout << "Zadejte sirku kmene: ";
+    cin >> sirka;
+
+    int vyska;
+    cout << "Zadejte vysku kmene: ";
+    cin >> vyska;
+
+    vytvoreniKoruny(vyska_stromu);
+    vytvoreniKmene(vyska, sirka, vyska_stromu);
+
+    
+    return 0;
+}
