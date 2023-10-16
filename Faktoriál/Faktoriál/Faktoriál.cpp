c1ddd1746dde9a9c362f861b8bceb501e4a1cb90
Ondrabegy1 <115577318+Ondrabegy1@users.noreply.github.com>
2023-10-16 10:36:01 +0200

    Add files via upload

---
+++ Faktoriál/Faktoriál/Faktoriál.cpp
@@ -0,0 +1,44 @@
+﻿//Dva způsoby řešení. Jeden přes cyklus a druhý přes rekurzní funkci.
+
+#include <iostream>
+
+using namespace std;
+
+//První způsob řešení (cyklus).
+/*void vypocetFaktorialu(int cislo)
+{
+    int vysledek = 1;
+
+    for (int i = 1; i <= cislo; i++)
+    {
+        vysledek *= i;
+    }
+
+    cout << vysledek << endl;
+}*/
+
+//Druhý způsob řešení (rekurze).
+int vypocetFaktorialu(int cislo)
+{
+    if (cislo == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return cislo * vypocetFaktorialu(cislo - 1);
+    }
+}
+
+int main()
+{
+    int cislo;
+    cout << "Zadejte cislo: ";
+    cin >> cislo;
+
+    vypocetFaktorialu(cislo);
+
+    cout << vypocetFaktorialu(cislo) << endl;
+
+    return 0;
+}
