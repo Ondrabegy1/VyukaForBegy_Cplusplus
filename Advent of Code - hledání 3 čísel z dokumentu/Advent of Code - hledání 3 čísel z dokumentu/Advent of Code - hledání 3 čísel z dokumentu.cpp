492ff76cbc510585be4de62fb1e8cc3f2f846eea
Ondrabegy1 <115577318+Ondrabegy1@users.noreply.github.com>
2023-10-17 10:26:51 +0200

    Add files via upload

---
+++ Advent of Code - hledání 3 čísel z dokumentu/Advent of Code - hledání 3 čísel z dokumentu/Advent of Code - hledání 3 čísel z dokumentu.cpp
@@ -0,0 +1,37 @@
+﻿//V tomto programu se hledají 3 čísla z dokumentu, která dohromady dávají 2020 a vypíše jejich součin.
+//Název souboru je "2020.txt".
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ifstream file("2020.txt");
+    vector<int> numbers;
+    int number;
+    while (file >> number) {
+        numbers.push_back(number);
+    }
+    sort(numbers.begin(), numbers.end());
+    for (int i = 0; i < numbers.size(); i++) {
+        int left = i + 1;
+        int right = numbers.size() - 1;
+        while (left < right) {
+            int sum = numbers[i] + numbers[left] + numbers[right];
+            if (sum == 2020) {
+                cout << numbers[i] << " * " << numbers[left] << " * " << numbers[right] << " = " << numbers[i] * numbers[left] * numbers[right] << "\n";
+                return 0;
+            }
+            else if (sum < 2020) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+    }
+}
