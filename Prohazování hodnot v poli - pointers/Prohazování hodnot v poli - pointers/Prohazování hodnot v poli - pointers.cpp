e12b931b632ae50665ac4a8f018edd73b5637278
Ondrabegy1 <115577318+Ondrabegy1@users.noreply.github.com>
2023-10-18 10:19:33 +0200

    Add files via upload

---
+++ Prohazování hodnot v poli - pointers/Prohazování hodnot v poli - pointers/Prohazování hodnot v poli - pointers.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void prohodPromene(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main()
+{
+	int a;
+	int b;
+
+	cout << "Zadejte a: ";
+	cin >> a;
+	cout << "Zadejte b: ";
+	cin >> b;
+	cout << endl;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl << endl;
+
+	prohodPromene(&a, &b);
+
+	cout << "a = " << a << " po prohozeni" << endl;
+	cout << "b = " << b << " po prohozeni" << endl;
+
+	return 0;
+}
