4a0d12d84472016ea6785cbd9cee39c22719d5ad
Ondrabegy1 <115577318+Ondrabegy1@users.noreply.github.com>
2024-01-09 10:11:47 +0100

    Add files via upload

---
+++ Matice/Matice/Matice.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+struct Matice
+{
+	int radky;
+	int sloupce;
+	int** matice;
+};
+
+Matice* initMatrix(int radky, int sloupce)
+{
+	Matice* matice = new Matice;
+	matice->radky = radky;
+	matice->sloupce = sloupce;
+	matice->matice = new int* [radky];
+
+	for (int i = 0; i < radky; i++)
+	{
+		matice->matice[i] = new int[sloupce];
+	}
+
+	return matice;
+}
+
+int** initArray(int radky, int sloupce)
+{
+	int** matice = new int* [radky];
+
+	for (int i = 0; i < radky; i++)
+	{
+		matice[i] = new int[sloupce];
+	}
+
+	return matice;
+}
+void sestrojMatici(int** matice, int radky, int sloupce)
+{
+	for (int i = 0; i < radky; i++)
+	{
+		for (int j = 0; j < sloupce; j++)
+		{
+			std::cout << "Zadej hodnotu pro [" << i << "][" << j << "]: ";
+			std::cin >> matice[i][j];
+		}
+	}
+
+	std::cout << std::endl << "Vypis matice:" << std::endl;
+
+	for (int i = 0; i < radky; i++)
+	{
+		for (int j = 0; j < sloupce; j++)
+		{
+			std::cout << matice[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+int main()
+{
+	int radky, sloupce;
+
+	std::cout << "Zadej pocet radku: ";
+	std::cin >> radky;
+	std::cout << "Zadej pocet sloupcu: ";
+	std::cin >> sloupce;
+	std::cout << std::endl;
+
+	int** matice = initArray(radky, sloupce);
+	sestrojMatici(matice, radky, sloupce);
+
+	return 0;
+}
---
+++ Odkazování struktur na sebe/Odkazování struktur na sebe/Odkazování struktur na sebe.cpp
@@ -0,0 +1,201 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct struktura
+{
+	int hodnota;
+	struktura* ukazatel;
+};
+
+void printStruktury(int* arr, int pocet);
+void printList(struktura* str);
+void deleteList(struktura* str);
+
+int* randomStruktury(int pocet);
+
+struktura* newStruktura(int hodnota);
+struktura* randomList(int* arr, int pocet);
+struktura* findStrukturu(struktura* str, int ukazatel);
+void deleteStrukturu(struktura** str, int ukazatel);
+struktura* addStrukturu(struktura* str, int ukazatel, int hodnota);
+
+int main()
+{
+	int pocet, ukazatel, hodnota;
+
+	cout << "Zadejte pocet struktur: ";
+	cin >> pocet;
+
+	int* arr = randomStruktury(pocet);
+	struktura* str = randomList(arr, pocet);
+
+	cout << "Pole: ";
+	printStruktury(arr, pocet);
+
+	cout << "Seznam: ";
+	printList(str);
+
+	cout << "Zadejte ukazatel: ";
+	cin >> ukazatel;
+
+	cout << "Zadejte hodnotu: ";
+	cin >> hodnota;
+
+	struktura* tmp = findStrukturu(str, ukazatel);
+
+	if (tmp != NULL)
+	{
+		cout << "Nalezena struktura: " << tmp->hodnota << endl;
+	}
+	else
+	{
+		cout << "Struktura nenalezena" << endl;
+	}
+
+	str = addStrukturu(str, ukazatel, hodnota);
+
+	cout << "Seznam: ";
+	printList(str);
+
+	deleteStrukturu(&str, ukazatel);
+
+	cout << "Seznam: ";
+	printList(str);
+
+	deleteList(str);
+	delete[] arr;
+
+	return 0;
+}
+
+void printStruktury(int* arr, int pocet)
+{
+	for (int i = 0; i < pocet; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+}
+
+void printList(struktura* str)
+{
+	while (str != NULL)
+	{
+		cout << str->hodnota << " ";
+		str = str->ukazatel;
+	}
+
+	cout << endl;
+}
+
+void deleteList(struktura* str)
+{
+	struktura* tmp;
+
+	while (str != NULL)
+	{
+		tmp = str;
+		str = str->ukazatel;
+		delete tmp;
+	}
+}
+
+int* randomStruktury(int pocet)
+{
+	int* arr = new int[pocet];
+
+	for (int i = 0; i < pocet; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+
+	return arr;
+}
+
+struktura* newStruktura(int hodnota)
+{
+	struktura* str = new struktura;
+	str->hodnota = hodnota;
+	str->ukazatel = NULL;
+
+	return str;
+}
+
+struktura* randomList(int* arr, int pocet)
+{
+	struktura* str = newStruktura(arr[0]);
+	struktura* tmp = str;
+
+	for (int i = 1; i < pocet; i++)
+	{
+		tmp->ukazatel = newStruktura(arr[i]);
+		tmp = tmp->ukazatel;
+	}
+
+	return str;
+}
+
+struktura* findStrukturu(struktura* str, int ukazatel)
+{
+	while (str != NULL)
+	{
+		if (str->hodnota == ukazatel)
+		{
+			return str;
+		}
+
+		str = str->ukazatel;
+	}
+
+	return NULL;
+}
+
+void deleteStrukturu(struktura** str, int ukazatel)
+{
+	struktura* tmp = *str;
+
+	if (tmp->hodnota == ukazatel)
+	{
+		*str = tmp->ukazatel;
+		delete tmp;
+		return;
+	}
+
+	while (tmp->ukazatel != NULL)
+	{
+		if (tmp->ukazatel->hodnota == ukazatel)
+		{
+			struktura* tmp2 = tmp->ukazatel;
+			tmp->ukazatel = tmp->ukazatel->ukazatel;
+			delete tmp2;
+			return;
+		}
+
+		tmp = tmp->ukazatel;
+	}
+}
+
+struktura* addStrukturu(struktura* str, int ukazatel, int hodnota)
+{
+	struktura* tmp = str;
+
+	while (tmp != NULL)
+	{
+		if (tmp->hodnota == ukazatel)
+		{
+			struktura* tmp2 = tmp->ukazatel;
+			tmp->ukazatel = newStruktura(hodnota);
+			tmp->ukazatel->ukazatel = tmp2;
+			return str;
+		}
+
+		tmp = tmp->ukazatel;
+	}
+
+	return NULL;
+}
